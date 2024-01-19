422b43c0a13381a0c4c54b40516feaee0c444b92
sam-mg <147686318+sam-mg@users.noreply.github.com>
2024-01-19 20:30:21 +0530

    Completed Reversing - 2

---
+++ Phase(2)/Reversing/Ch-2/Working/To Get Flag.py
@@ -0,0 +1,33 @@
+# uncompyle6 version 3.9.0
+# Python bytecode version base 2.7 (62211)
+# Decompiled from: Python 3.8.10 (default, Nov 22 2023, 10:22:35) 
+# [GCC 9.4.0]
+# Embedded file name: ./chall.py
+# Compiled at: 2024-01-18 13:14:06
+
+
+def modify_string(input_str):
+    modified_str = ''
+    for i in range(len(input_str)):
+        char = input_str[i]
+        if i % 2 == 0:
+            modified_str += chr(ord(char) + 3)
+        else:
+            modified_str += chr(ord(char) - 2)
+
+    return modified_str
+
+
+def main():
+    user_input = input('Enter a string: ')
+    modified_input = modify_string(user_input)
+    print(modified_input)
+    # target_string = 'cn^ixelomkigaam{qjlp<Az'
+    # if modified_input == target_string:
+    #     print ('Congratulations! You got the correct modified string.')
+    # else:
+    #     print ("Sorry, the modified string doesn't match the target.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ Phase(2)/Reversing/Ch-2/Write-up.md
@@ -0,0 +1,17 @@
+Found out it was Python2.7 file.
+Searched how to open it.
+Found Headache.
+
+Final Go:
+Searched online decompilers.
+[Decompiler](https://www.decompiler.com)
+
+Got the decompiled code online.
+[Decompiled_Code](Reversing/Ch-2/Working/Decompiled.py)
+
+Make a few changes in the code.
+[Code_To_Get_Flag](Reversing/Ch-2/Working/To Get Flag.py)
+
+Got the FLAG
+
+- flag{compiled_python??}
